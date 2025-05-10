64072f8e1613215702f4bd4bae5d6a16f6485e37
DvopHeh <dvoppe@yahoo.com>
2025-05-10 11:08:03 +0200

    new file:   .gitignore new file:   .vscode/extensions.json new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   astro.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   src/components/Navbar.astro 	new file:   src/components/Page.astro 	new file:   src/layouts/Layout.astro 	new file:   src/pages/index.astro 	new file:   styles/globals.css 	new file:   tsconfig.json

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    //server: {
+    //    port: 4321,
+    //    allowedHosts: [""], // add your allowed hosts here
+    //},
+});
