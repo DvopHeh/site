67d5b5b7f0649defe054e910f5cc61768e7c5b31
DvopHeh <dvoppe@yahoo.com>
2025-07-08 00:59:49 +0200

    penis epic mega update
    Co-authored-by: Charlie <vorlie@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
     server: {
         port: 4321,
-        allowedHosts: ["dvop.vorlie.pl"],
+    adapter: cloudflare(),
     },
 });
