298a6d02e1f6ae74ba4ac4010ee55cd2fc1f77e9
vorlie <vorlie0961@gmail.com>
2025-07-09 02:56:48 +0200

    aaah

--- src/scripts/lanyard/lanyard.ts
+++ src/scripts/lanyard/lanyard.ts
@@ -3,7 +3,7 @@ import type { PresenceData } from '../../types/interfaces';
 
 export const fetchPresence = async (): Promise<PresenceData | null> => {
     try {
-        const response = await fetch('https://api.lanyard.rest/v1/users/614807913302851594');
+        const response = await fetch('https://api.lanyard.rest/v1/users/410475909125242901');
         if (!response.ok) throw new Error(`Error: ${response.status}`);
         const { data }: { data: PresenceData } = await response.json();
         return data;
