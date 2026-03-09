2c7ce6426ab0253b465156596b70748167365863
DvopHeh <dvoppe@yahoo.com>
2026-03-09 18:10:03 +0100

    added a friend button idk

--- src/lib/components/home/FriendsSection.svelte
+++ src/lib/components/home/FriendsSection.svelte
@@ -1,7 +1,13 @@
 <section class="landing-section">
   <h2 class="landing-section-title">My friends</h2>
   <div class="friend-buttons">
-    <img src="/images/frank.png" alt="my button" width="88" height="31" loading="lazy" />
+    <img
+      src="/images/frank.png"
+      alt="my button"
+      width="88"
+      height="31"
+      loading="lazy"
+    />
     <a href="https://vorlie.pl/" target="_blank" rel="noopener noreferrer">
       <img
         src="https://vorlie.pl/images/vorlie.png"
@@ -21,7 +27,13 @@
       />
     </a>
     <a href="https://aveline.fish/" target="_blank" rel="noopener noreferrer">
-      <img src="/images/velbutton.gif" alt="Vel Button" width="88" height="31" loading="lazy" />
+      <img
+        src="/images/velbutton.gif"
+        alt="Vel Button"
+        width="88"
+        height="31"
+        loading="lazy"
+      />
     </a>
     <a href="https://byeoon.dev" target="_blank" rel="noopener noreferrer">
       <img
@@ -33,13 +45,24 @@
       />
     </a>
     <a href="https://pre1ude.dev" target="_blank" rel="noopener noreferrer">
-      <img src="https://pre1ude.dev/blankie.png" alt="pre1ude.dev" height="31" width="88" />
+      <img
+        src="https://pre1ude.dev/blankie.png"
+        alt="pre1ude.dev"
+        height="31"
+        width="88"
+      />
     </a>
     <iframe
       sandbox="allow-top-navigation-by-user-activation"
       style="height: 31px; width: 88px; overflow: hidden; border: none"
       src="https://salamithecat.com/button"
       title="peeblyweeb"
+    ></iframe>
+    <iframe
+      sandbox="allow-top-navigation-by-user-activation"
+      style="height: 31px; width: 88px; overflow: hidden; border: none"
+      src="https://nekos.space/button.html"
+      title="nekos.space"
     ></iframe>
   </div>
 </section>
